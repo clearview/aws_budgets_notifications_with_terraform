cd8500944a52569ec2f5e777d564a3f066c7ac49
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2024-01-02 21:25:33 +0100

    add monthly_account_budget TF

--- budgets.tf
+++ budgets.tf
@@ -1,27 +1,3 @@
-resource "aws_budgets_budget" "my_account_monthly_budget" {
-  name         = "Monthly Budget for my Account"
-  budget_type  = "COST"
-  limit_amount = "500"
-  limit_unit   = "USD"
-  time_unit    = "MONTHLY"
-
-  notification {
-    comparison_operator        = "GREATER_THAN"
-    threshold                  = 90
-    threshold_type             = "PERCENTAGE"
-    notification_type          = "ACTUAL"
-    subscriber_email_addresses = ["email@my.team"]
-  }
-
-  notification {
-    comparison_operator        = "GREATER_THAN"
-    threshold                  = 110
-    threshold_type             = "PERCENTAGE"
-    notification_type          = "FORECASTED"
-    subscriber_email_addresses = ["email@my.team"]
-  }
-}
-
 resource "aws_budgets_budget" "my_ec2_monthly_budget" {
   name         = "EC2 Monthly Budget"
   budget_type  = "COST"
---
+++ monthly_account_budget.tf
@@ -0,0 +1,23 @@
+resource "aws_budgets_budget" "monthly_account_budget" {
+  name         = "Monthly Budget for my Account"
+  budget_type  = "COST"
+  limit_amount = "500"
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 90
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = ["email@my.team"]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 110
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = ["email@my.team"]
+  }
+}
