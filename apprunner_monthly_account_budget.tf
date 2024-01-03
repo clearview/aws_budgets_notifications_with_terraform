59f7b8f1dcdf88d528d2387b2187671abc642c4a
Nedim Hadzimahmutovic <h.nedim@gmail.com>
2024-01-03 08:34:27 +0100

    add apprunner_monthly_budget

---
+++ apprunner_monthly_account_budget.tf
@@ -0,0 +1,30 @@
+resource "aws_budgets_budget" "apprunner_monthly_budget" {
+  name         = "AppRunner Monthly Budget"
+  budget_type  = "COST"
+  limit_amount = "100"
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+
+  cost_filter {
+    name = "Service"
+    values = [
+      "AWS App Runner",
+    ]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 90
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "ACTUAL"
+    subscriber_email_addresses = ["email@my.team"]
+  }
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 110
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = ["email@my.team"]
+  }
+}
